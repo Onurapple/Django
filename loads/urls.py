e5bf7622b7b2a3563bb465b6bb8e35f7a939b29a
Onur <honorclarus2000@outlook.com>
2023-03-11 16:10:35 +0300

    loads app set. with class based templete views.

---
+++ loads/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ListView
+
+urlpatterns = [
+    path('', ListView.as_view(), name='load_list'),
+    
+
+]
--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^eyiaa0fzw--f9%==3b4#a!bcaqgcw(l1ant$gvu=1+fp@z3ug'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'loads'
 ]
 
 MIDDLEWARE = [
